6d1b993d5d78beb45057a98e8d7ae78a08e993c8
Peyton Duncan <peyton.duncan45@gmail.com>
2016-11-28 12:33:59 -0500

    Inital GUI building, partially implemented button

---
+++ pokemon-compare-csharp-gui/pokemon-compare-csharp-gui/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+using System.Runtime.Remoting.Messaging;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnGoButtonClicked (object sender, EventArgs e)
+	{
+		outputText.Markup = "<u>TESTING</u><b>TESTING</b>";
+	}
+}
---
+++ pokemon-compare-csharp-gui/pokemon-compare-csharp-gui/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace pokemoncomparecsharpgui
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("STARTED!");
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
